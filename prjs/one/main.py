ecb52375921b4ab379e17c8af5a22aeaa3f1e281
Oleg Jarma Montoya <ojarmam@unal.edu.co>
2024-09-12 16:48:57 +0200

    update project 1

--- prjs/one/main.py
+++ prjs/one/main.py
@@ -1,7 +1,7 @@
 # %% Imports (add as needed) #############################################
 from functools import partial
 import gymnasium as gym  # not jax based
-from jax import random, nn, jit, vmap
+from jax import random, nn, jit, vmap, value_and_grad, tree
 from jax._src.api import jit
 import jax.numpy as jnp
 from numpy.random.mtrand import gamma
@@ -9,53 +9,61 @@ from tqdm import tqdm
 from collections import deque, namedtuple
 import chex
 from copy import deepcopy
+from random import sample
 
 # %% Constants ###########################################################
-env = gym.make("MountainCar-v0", render_mode="human")  #  render_mode="human")
+env = gym.make("CartPole-v1", render_mode="human")  #  render_mode="human")
 rng = random.PRNGKey(1331)
 entry = namedtuple("Memory", ["obs", "action", "reward", "next_obs", "done"])
 memory = deque(maxlen=1000)  # <- replay buffer
-gamma = 0.99
-epsilon = 0.2
+gamma = 0.95
+epsilon = 0.3
+epsilon_decay = 0.01
 sample_size = 32
+learning_rate = 0.005
 # define more as needed
-# %%
-print(env.observation_space.__dict__)
+
 
 
 # %% Model ###############################################################
-rng, *keys = random.split(rng, 10)
 
 
-@chex.dataclass
-class Params:
+
+# @chex.dataclass
+# class Params:
+    # w1 = random.normal(keys[0], (env.observation_space._shape[0], 16))
+    # b1 = random.normal(keys[1], 16)
+    # w2 = random.normal(keys[2], (16, 16))
+    # b2 = random.normal(keys[3], 16)
+    # w3 = random.normal(keys[4], (16, env.action_space.n))
+    # b3 = random.normal(keys[5], int(env.action_space.n))
+#     def __iter__(self):
+#             yield self.w1
+#             yield self.b1
+#             yield self.w2
+#             yield self.b2
+#             yield self.w3
+#             yield self.b3
+def init_params(rng, env):
+    rng, *keys = random.split(rng, 10)
     w1 = random.normal(keys[0], (env.observation_space._shape[0], 16))
     b1 = random.normal(keys[1], 16)
     w2 = random.normal(keys[2], (16, 16))
     b2 = random.normal(keys[3], 16)
     w3 = random.normal(keys[4], (16, env.action_space.n))
     b3 = random.normal(keys[5], int(env.action_space.n))
+    return [w1, b1, w2, b2, w3, b3]
 
-test_params = Params()
-
-print(test_params.w3.shape)
-def model(params: Params, x_data):
-    z = x_data @ params.w1 + params.b1
+def model(params, x_data):
+    z = x_data @ params[0] + params[1]
     z = nn.relu(z)
-    z = z @ params.w2 + params.b2
+    z = z @ params[2] + params[3]
     z = nn.relu(z)
-    z = z @ params.w3 + params.b3
+    z = z @ params[4] + params[5]
     z = nn.softmax(z)
 
     # return jnp.argmax(z)
     return z
-
-
-# @partial(vmap, in_axes=(None, None, None, 0, 0, 0, None, 0))
-def td_loss(params, target_params, model, obs, next_obs, action, gamma, reward):
-    target = gamma * jnp.max(model(target_params, next_obs))
-    prediction = model(params, obs)[action]
-    return (reward + target - prediction)**2
 
 
 def random_policy_fn(rng): # action (shape: ())
@@ -66,21 +74,19 @@ def e_greedy_policy_fn(rng, q_a, epsilon, decay=0.99):  # obs (shape: (2,)) to a
     rng, key = random.split(rng)
     random_action = random_policy_fn(rng)
     action = jnp.argmax(q_a)
-    final_action = random.choice(key, jnp.array([action, random_action]), p=jnp.array([1-epsilon*decay, epsilon*decay]))
+    final_action = random.choice(key, jnp.array([action, random_action]), p=jnp.array([1-epsilon, epsilon]))
     return int(final_action)
 
-# def update():
-#     raise NotImplementedError
+def batch_td_loss(params, target_params, model, gamma, obs, action, reward, next_obs, done):
 
-# %% test
-rng, key = random.split(rng)
-obs, info = env.reset()
-q_a = model(Params(), obs)
-action = e_greedy_policy_fn(key, q_a, epsilon)
-next_obs, reward, terminated, truncated, info = env.step(action)
-print(td_loss(Params(), Params(), model, obs, next_obs, action, gamma, reward))
-# env.close()
+    @partial(vmap, in_axes=(None, None, None, None, 0, 0, 0, 0, 0))
+    def td_loss(params, target_params, model, gamma, obs, action, reward, next_obs, done):
+        target = gamma * jnp.max(model(target_params, next_obs))
+        prediction = model(params, obs)[action]
+        # return target
+        return jnp.square(reward + (1 - done) * target - prediction)
 
+    return jnp.mean(td_loss(params, target_params, model, gamma, obs, action, reward, next_obs, done))
 
 # %% Environment #########################################################
 def random_play(rng, steps):
@@ -94,21 +100,55 @@ def random_play(rng, steps):
     env.close()
 
 
-random_play(rng, 100)
-
+# random_play(rng, 100)
 
 # %% trainig
-def train(rng, model, params, steps, gamma, epsilon, t, sample_size):
+def train(rng, model, params, steps, gamma, epsilon, t, sample_size, learning_rate):
     obs, info = env.reset()
-    target_params = params.deepcopy()
-    for i in tqdm(range(steps)):
-        rng, key = random.split(rng)
+    avg_reward = []
+    target_params = params
+    done = False
+    for i in range(1,steps+1):
+        while not done:
+            rng, key = random.split(rng)
 
-        q_a = model(params, obs)
-        action = e_greedy_policy_fn(rng, q_a, epsilon)
-        next_obs, reward, terminated, truncated, info = env.step(action)
-        memory.append(entry(obs, action, reward, next_obs, terminated | truncated))
+            q_a = model(params, obs)
+            action = e_greedy_policy_fn(rng, q_a, epsilon, )
+            next_obs, reward, terminated, truncated, info = env.step(action)
+            avg_reward.append(reward)
+            done = terminated | truncated
+            memory.append(entry(obs, action, reward, next_obs, done))
+
+
         if len(memory) < sample_size:
+            batch = sample(memory, len(memory))
+        else:
+            batch = sample(memory, sample_size)
+
+        obs_batch = jnp.array([x.obs for x in batch])
+        action_batch = jnp.array([x.action for x in batch])
+        reward_batch = jnp.array([x.reward for x in batch])
+        next_obs_batch = jnp.array([x.next_obs for x in batch])
+        done_batch = jnp.array([x.done for x in batch])
 
 
-        obs, info = next_obs, info if not (terminated | truncated) else env.reset()
+        loss, grads = value_and_grad(batch_td_loss)(params, target_params, model, gamma, obs_batch, action_batch, reward_batch, next_obs_batch, done_batch)
+        # params = tree.map(lambda p, g: p -  g * learning_rate, params, grads)
+        params = [params[i] - learning_rate * grads[i] for i in range(len(params))]
+        # print(params[0])
+        if i % t == 0:
+            target_params = params
+            # print(f"Step: {i}, Average reward: {jnp.mean(jnp.array(avg_reward))}")
+            print(params[0])
+
+        obs, info = env.reset()
+
+    env.close()
+
+env = gym.make("CartPole-v1", render_mode="human")  #  render_mode="human")
+
+params = init_params(rng, env)
+
+print(params[0].shape)
+
+train(rng, model, params, 10000, gamma, epsilon,100, 32, learning_rate)
